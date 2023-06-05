45d910c731cd489b6a0eff79f0f63033293be0f9
Nana Gaisie <107763933+NANAGAISIE1@users.noreply.github.com>
2023-06-05 17:23:42 +0000

    Change on Hero Section

--- HTML-CSS/tailwind.config.js
+++ HTML-CSS/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./public/painter.jpg')",
+      },
+    },
   },
   plugins: {
     tailwindcss: {},
