0875048e8a0bb8f63c64ee6b4f555edae847d007
nana gaisie <nabgaisie@outlook.com>
2023-07-08 19:27:01 +0000

    painter deletion

--- HTML-CSS/tailwind.config.js
+++ HTML-CSS/tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero-pattern': "url('./public/painter.jpg')",
-      },
+      backgroundImage: {},
     },
   },
   plugins: {
